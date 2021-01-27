b2467e6d20f39d5c828f04f911d8209632bc532c
mgxmajewski <mgxmajewski@gmail.com>
2021-01-27 23:34:08 +0000

    Count words

--- readability.py
+++ readability.py
@@ -11,3 +11,11 @@ def lettersCount():
         if ((givenText[i]).isalpha()):
            letters += 1
     return letters
+    
+# Counts words
+def wordsCount():
+    words = 0
+    for i in range(textLentgh):
+        if (givenText[i] == " "):
+            words+= 1
+    return words + 1
