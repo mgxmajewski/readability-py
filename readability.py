c024e3a810f3bb07dba1d486766ae4ecef926188
mgxmajewski <mgxmajewski@gmail.com>
2021-01-27 23:53:28 +0000

    Refactor - improve formating

--- readability.py
+++ readability.py
@@ -1,34 +1,38 @@
 from cs50 import get_string
 
 # Get text to analize from user
-givenText = get_string("Text: ");
+givenText = get_string("Text: ")
 textLentgh = len(givenText)
+
 
 # Count letters
 def lettersCount():
     letters = 0
     for i in range(textLentgh):
         if ((givenText[i]).isalpha()):
-           letters += 1
+            letters += 1
     return letters
+    
     
 # Count words
 def wordsCount():
     words = 0
     for i in range(textLentgh):
         if (givenText[i] == " "):
-            words+= 1
+            words += 1
     return words + 1
+
 
 # Count sentences
 def sentencesCount():
     sentences = 0
-    for i in range (textLentgh):
-        if (givenText[i] == "." or givenText[i] == "!" or  givenText[i] == "?"):
-            sentences+=1
+    for i in range(textLentgh):
+        if (givenText[i] == "." or givenText[i] == "!" or givenText[i] == "?"):
+            sentences += 1
     return sentences
-    
-# Calling functions to compute index
+
+
+# Call functions to pass to indexColemanLiau
 letters = lettersCount()
 words = wordsCount()
 sentences = sentencesCount()
@@ -41,10 +45,12 @@ def indexColemanLiau():
     index = 0.0588 * l - 0.296 * s - 15.8
     return index
     
+
 # Display result refferenced to Grade
+index = round(indexColemanLiau())
 if indexColemanLiau() < 1:
     print("Before Grade 1")
 elif indexColemanLiau() > 16:
     print("Grade 16+")
 else:
-    print("Grade " + str(round(indexColemanLiau())))
+    print("Grade " + str(index))
