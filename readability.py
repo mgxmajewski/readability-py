82ddcbd9498f99aadd8ae5c962da0642f58e3c4e
mgxmajewski <mgxmajewski@gmail.com>
2021-01-27 23:33:27 +0000

    Count letters

--- readability.py
+++ readability.py
@@ -3,3 +3,11 @@ from cs50 import get_string
 # Get text to analize from user
 givenText = get_string("Text: ");
 textLentgh = len(givenText)
+
+# Count letters
+def lettersCount():
+    letters = 0
+    for i in range(textLentgh):
+        if ((givenText[i]).isalpha()):
+           letters += 1
+    return letters
